a549656f3202b759eea65b1054d4c09b017dc98b
wumba <j.antonius@gmx.net>
2023-02-02 10:04:52 +0100

    add test test

--- leistungstraeger-app/src/test/java/application/TestTest.java
+++ leistungstraeger-app/src/test/java/application/TestTest.java
@@ -1,4 +1,13 @@
 package application;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestTest {
+
+    @Test
+    public void test() {
+        assertEquals(3,3);
+    }
 }
