c37f843a67da99bbdd2043047594986513cacc66
christ_sve <chrsve@gmx.de>
2023-02-09 10:52:03 +0100

    Remove methods from interface

--- leistungstraeger-app/src/main/java/object/interaction/HasInventory.java
+++ leistungstraeger-app/src/main/java/object/interaction/HasInventory.java
@@ -1,21 +1,11 @@
 package object.interaction;
 
 import item.AbstractItem;
+import item.ItemStash;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface HasInventory {
-
-    List<AbstractItem> getInventory();
-
-    AbstractItem removeItem(UUID id);
-
-    List<AbstractItem> removeAllItems();
-
-    void putItem(AbstractItem item);
-
-    void putItems(List<AbstractItem> items);
-
-
+    ItemStash getInventory();
 }
