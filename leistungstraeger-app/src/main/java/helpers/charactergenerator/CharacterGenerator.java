ef9c092c3a1b02595c8d6caaf70b969340f7cd38
wumba <j.antonius@gmx.net>
2023-02-23 10:35:19 +0100

    fix merge issues

--- leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
+++ leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
@@ -12,27 +12,27 @@ public class CharacterGenerator {
 
     public RingList<GameCharacter> generateCharacters(Client client) {
         RingList<GameCharacter> list = new RingList<>();
-        list.add(new GameCharacter(client,"Burbl", new Coordinate(2, 2), "character.png", 2, 20,1));
-        list.add(new GameCharacter(new Client(2),"Dieter", new Coordinate(4, 2), "character.png",2, 20, 1));
-        Sword sword = new Sword("Hammer", new Modifier(ModifierIdentifier.DAMAGE, 200));
-        GameCharacter character = new GameCharacter(
+        Sword sword = new Sword("Hammer",
+                new Modifier(ModifierIdentifier.DAMAGE, 200));
+        GameCharacter character1 = new GameCharacter(
                 client,
-                "David",
-                new Coordinate(2, 2),
-                "character",
+                "Dieter",
+                new Coordinate(4, 2),
+                "character.png",
                 2,
                 20,
                 1);
-        character.setItem(sword);
-        list.add(character);
-        list.add(new GameCharacter(
+        GameCharacter character2 = new GameCharacter(
                 new Client(2),
-                "David2",
-                new Coordinate(4, 2),
-                "character",
+                "David",
+                new Coordinate(2, 2),
+                "character.png",
                 2,
                 20,
-                1));
+                1);
+        character1.setItem(sword);
+        list.add(character1);
+        list.add(character2);
         return list;
     }
 }
