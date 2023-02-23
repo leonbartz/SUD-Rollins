095077b24921e812bf02742c255742677ed0dd78
wumba <j.antonius@gmx.net>
2023-02-23 10:29:29 +0100

    fix merge issues

--- leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
+++ leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
@@ -1,7 +1,10 @@
 package helpers.charactergenerator;
 
-import core.character.GameCharacter;
 import core.client.Client;
+import core.item.implementations.Sword;
+import core.item.modifier.Modifier;
+import core.item.modifier.ModifierIdentifier;
+import core.object.implementation.GameCharacter;
 import helpers.collections.RingList;
 import helpers.coordinate.Coordinate;
 
@@ -9,8 +12,8 @@ public class CharacterGenerator {
 
     public RingList<GameCharacter> generateCharacters(Client client) {
         RingList<GameCharacter> list = new RingList<>();
-        list.add(new GameCharacter(client, new Coordinate(2, 2), "character.png", 20, 1));
-        list.add(new GameCharacter(new Client(2), new Coordinate(4, 2), "character.png", 20, 1));
+        list.add(new GameCharacter(client,"Burbl", new Coordinate(2, 2), "character.png", 2, 20,1));
+        list.add(new GameCharacter(new Client(2),"Dieter", new Coordinate(4, 2), "character.png",2, 20, 1));
         Sword sword = new Sword("Hammer", new Modifier(ModifierIdentifier.DAMAGE, 200));
         GameCharacter character = new GameCharacter(
                 client,
