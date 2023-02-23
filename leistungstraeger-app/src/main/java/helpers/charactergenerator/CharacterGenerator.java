bef62da48e8454cae5d2dc1bac24ee6205d65c59
christ_sve <chrsve@gmx.de>
2023-02-23 09:06:47 +0100

    Add test sword

--- leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
+++ leistungstraeger-app/src/main/java/helpers/charactergenerator/CharacterGenerator.java
@@ -1,5 +1,8 @@
 package helpers.charactergenerator;
 
+import core.item.implementations.Sword;
+import core.item.modifier.Modifier;
+import core.item.modifier.ModifierIdentifier;
 import core.object.implementation.GameCharacter;
 import core.client.Client;
 import helpers.collections.RingList;
@@ -9,14 +12,17 @@ public class CharacterGenerator {
 
     public RingList<GameCharacter> generateCharacters(Client client) {
         RingList<GameCharacter> list = new RingList<>();
-        list.add(new GameCharacter(
+        Sword sword = new Sword("Hammer", new Modifier(ModifierIdentifier.DAMAGE, 200));
+        GameCharacter character = new GameCharacter(
                 client,
                 "David",
                 new Coordinate(2, 2),
                 "character",
                 2,
                 20,
-                1));
+                1);
+        character.setItem(sword);
+        list.add(character);
         list.add(new GameCharacter(
                 new Client(2),
                 "David2",
