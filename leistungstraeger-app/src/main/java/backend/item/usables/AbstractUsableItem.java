3d246ba244fe9a69a4121fee77d28a3b0f36e532
christ_sve <chrsve@gmx.de>
2023-03-02 10:16:51 +0100

    Implement methods

--- leistungstraeger-app/src/main/java/backend/item/usables/AbstractUsableItem.java
+++ leistungstraeger-app/src/main/java/backend/item/usables/AbstractUsableItem.java
@@ -2,14 +2,13 @@ package backend.item.usables;
 
 import backend.item.AbstractModifyingItem;
 import backend.item.ItemStash;
-import backend.item.ItemUtils;
 import backend.item.modifier.Modifier;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
-import static backend.item.usables.ItemActivationType.*;
+import static backend.item.usables.ItemActivationType.COUNT_ROUNDS;
+import static backend.item.usables.ItemActivationType.ON_REACTIVATION;
 
 
 /**
@@ -19,43 +18,42 @@ import static backend.item.usables.ItemActivationType.*;
  */
 public class AbstractUsableItem extends AbstractModifyingItem {
 
-    // (non)permanent item deactivation -> cannot be used while empty
+    // item can only be used if not used up
     @Getter
-    private boolean isEmpty = false;
+    private boolean usedUp = false;
 
     @Getter
     private final ItemActivationType activationType;
 
-    // TODO remaining usages implementieren -> ItemActivityType
+    @Getter
+    private final ItemActivityType activityType;
 
     // Whether effect is currently active
-    // TODO effekt aktiv für x runden -> danach empty
-    // TODO das muss überarbeitet werden, oder der kommentar muss angepasst werden
     @Getter
-    @Setter
     private boolean isActive = true;
 
     // The amount of turns after which the item activates again
-    private final int resetCounter;
+    private final int cooldown;
 
-    // If inactive, counts how many inactive turns have passed
-    private int inactiveRounds = 0;
+    // How many turns the item can stay active
+    private final int activityTime;
 
-    public AbstractUsableItem(final String name,
-                              final ItemActivationType type,
-                              final int resetCounter,
-                              final Modifier... modifiers) {
-        super(name, modifiers);
-        this.activationType = type;
-        this.resetCounter = resetCounter;
-    }
+    private int activityCounter = 0;
+
+    // Counts inactive rounds if ItemActivationType is COUNT_ROUNDS
+    private int cooldownCounter = 0;
 
     public AbstractUsableItem(final String name,
-                              final ItemActivationType type,
+                              final ItemActivationType activationType,
+                              final ItemActivityType activityType,
+                              final int cooldown,
+                              final int activityTime,
                               final Modifier... modifiers) {
         super(name, modifiers);
-        this.activationType = type;
-        this.resetCounter = 0;
+        this.activationType = activationType;
+        this.activityType = activityType;
+        this.cooldown = cooldown;
+        this.activityTime = activityTime;
     }
 
     /**
@@ -63,15 +61,22 @@ public class AbstractUsableItem extends AbstractModifyingItem {
      * remain or reactivates, if item has necessary activation type.
      */
     public void countTurn() {
-        if (isActive()) return;
+        // If item is permanently used up, don't count
+        if (!isUsedUp()) return;
 
-        // If inactive, either count up or reactivate item
-        if (activationType == COUNT_ROUNDS) {
-            if (inactiveRounds == resetCounter) {
-                inactiveRounds = 0;
-                setActive(true);
-            } else {
-                inactiveRounds++;
+        if (isActive() && activityType.equals(ItemActivityType.DEACTIVATES_AFTER_COUNT)) {
+            // Item is active and might deactivate
+            activityCounter++;
+            if (activityCounter == activityTime) {
+                isActive = false;
+                activityCounter = 0;
+            }
+        } else if (!isActive() && activationType.equals(COUNT_ROUNDS)) {
+            // Item is inactive and might be usable again
+            cooldownCounter++;
+            if (cooldownCounter == cooldown) {
+                usedUp = false;
+                cooldownCounter = 0;
             }
         }
     }
@@ -80,7 +85,7 @@ public class AbstractUsableItem extends AbstractModifyingItem {
      * Attempt to reactivate item. If necessary type is set, item is reactivated.
      */
     public void reactivate() {
-        if (getActivationType() == ON_REACTIVATION) setActive(true);
+        if (getActivationType() == ON_REACTIVATION) isActive = true;
     }
 
     /**
@@ -89,12 +94,11 @@ public class AbstractUsableItem extends AbstractModifyingItem {
      * @return - if active, list of effects, otherwise empty list.
      */
     public List<Modifier> use() {
-        if (isActive() && !isEmpty()) {
-            if (activationType == SINGLE_USE) isEmpty = true;
-            setActive(false);
+        if (!usedUp) {
+            usedUp = true;
+            if (!isActive) isActive = true;
             return ItemUtils.translateToModifier(getActiveModifiers());
         }
-
         return List.of();
     }
 }
