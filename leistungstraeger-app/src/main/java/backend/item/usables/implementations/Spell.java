7d15c87dedb02bf090897fe11753141630f3ac9e
christ_sve <chrsve@gmx.de>
2023-03-09 09:11:18 +0100

    Cleanup

--- leistungstraeger-app/src/main/java/backend/item/usables/implementations/Spell.java
+++ leistungstraeger-app/src/main/java/backend/item/usables/implementations/Spell.java
@@ -5,7 +5,7 @@ import backend.item.usables.AbstractUsableItem;
 import backend.item.usables.ItemActivationType;
 
 public class Spell extends AbstractUsableItem {
-    
+
     public Spell(final String name,
                  final ItemActivationType activationType,
                  final int cooldown,
