82950832482418613c7a9465d581bc1e9f0fde15
Giesen-Carl <70052513+Giesen-Carl@users.noreply.github.com>
2023-03-30 10:52:17 +0200

    Generate Map

--- leistungstraeger-app/src/main/java/helpers/mapgenerator/GameMapGenerator.java
+++ leistungstraeger-app/src/main/java/helpers/mapgenerator/GameMapGenerator.java
@@ -14,26 +14,52 @@ import java.util.List;
  */
 public class GameMapGenerator {
 
-    public GameMapGenerator() {
+    private final GameMap gameMap = new GameMap();
 
-    }
+    public GameMapGenerator() {}
 
     public GameMap generate(List<GameCharacter> characters){
-        GameMap gameMap = new GameMap();
-
-        Door door1 = new Door("floor_ladder.png", new Coordinate(5, 4), CardinalDirection.NORTH);
-        Door door2 = new Door("floor_ladder.png", new Coordinate(1, 1), CardinalDirection.EAST);
-        Door door3 = new Door("floor_ladder.png", new Coordinate(5, 10), CardinalDirection.EAST);
-        Door.linkDoors(door1, door2);
-        Room room1 = new Room(10, 10, new DungeonRoomStyle());
-        room1.add(characters);
-        room1.add(door1);
-        Room room2 = new Room(10, 3, new DungeonRoomStyle());
-        room2.add(door2);
-        gameMap.add(room1, room2);
-        gameMap.setActiveRoom(room1);
-
+        startRoom(characters);
+        hallway();
+        labyrinth();
+        trophyRoom();
         return gameMap;
+    }
+
+    private final Door door1 = new Door("floor_ladder.png", new Coordinate(5, 3), CardinalDirection.NORTH);
+    public void startRoom(List<GameCharacter> characters) {
+        Room room = new Room(7, 4, new DungeonRoomStyle());
+        room.add(characters);
+        room.add(door1);
+        gameMap.add(room);
+        gameMap.setActiveRoom(room);
+    }
+    private final Door door2 = new Door("floor_ladder.png", new Coordinate(0, 1), CardinalDirection.EAST);
+    private final Door door3 = new Door("floor_ladder.png", new Coordinate(7, 1), CardinalDirection.WEST);
+    public void hallway() {
+        Room room = new Room(8, 3, new DungeonRoomStyle());
+        room.add(door2);
+        room.add(door3);
+        Door.linkDoors(door1, door2);
+        gameMap.add(room);
+    }
+
+    private final Door door4 = new Door("floor_ladder.png", new Coordinate(4, 0), CardinalDirection.SOUTH);
+    private final Door door5 = new Door("floor_ladder.png", new Coordinate(9, 9), CardinalDirection.WEST);
+    public void labyrinth() {
+        Room room = new Room(10, 10, new DungeonRoomStyle());
+        room.add(door4);
+        room.add(door5);
+        Door.linkDoors(door3, door4);
+        gameMap.add(room);
+    }
+
+    private final Door door6 = new Door("floor_ladder.png", new Coordinate(1, 2), CardinalDirection.EAST);
+    public void trophyRoom() {
+        Room room = new Room(5, 5, new DungeonRoomStyle());
+        room.add(door6);
+        Door.linkDoors(door5, door6);
+        gameMap.add(room);
     }
 
 }
