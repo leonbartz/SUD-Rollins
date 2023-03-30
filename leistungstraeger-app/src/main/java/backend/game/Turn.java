59c887d86bb9f95c6a2310fd729696b65ae093b4
Giesen-Carl <70052513+Giesen-Carl@users.noreply.github.com>
2023-03-30 08:53:45 +0200

    Turn properties

--- leistungstraeger-app/src/main/java/backend/game/Turn.java
+++ leistungstraeger-app/src/main/java/backend/game/Turn.java
@@ -3,16 +3,26 @@ package backend.game;
 import backend.character.GameCharacter;
 import backend.network.client.Client;
 import lombok.Getter;
+import lombok.Setter;
+
 /*
 @author: Carl, Eric, Jacob, Jasper, Leon, Sven
  */
 public class Turn {
+    @Getter
+    @Setter
+    private int movement;
+    @Getter
+    @Setter
+    private int actions;
     @Getter
     private final GameCharacter turnCharacter;
     @Getter
     private final Client turnClient;
 
     public Turn(final GameCharacter turnPlayer) {
+        actions = 1;
+        movement = turnPlayer.getMaximumRange();
         this.turnCharacter = turnPlayer;
         this.turnClient = turnCharacter.getClient();
     }
