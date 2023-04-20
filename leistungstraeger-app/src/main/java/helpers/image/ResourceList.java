14e8d6ee3ea3250121d12ecdccb4ad6403fd8050
wumba <j.antonius@gmx.net>
2023-04-20 09:25:34 +0200

    import pictures as stream and hardcode them

--- leistungstraeger-app/src/main/java/helpers/image/ResourceList.java
+++ leistungstraeger-app/src/main/java/helpers/image/ResourceList.java
@@ -12,24 +12,49 @@ import java.util.Map;
 @author: Carl, Eric, Jacob, Jasper, Leon, Sven
  */
 public class ResourceList {
+    private final static String[] imageNames = new String[]{
+            "big_demon_idle_anim_f0.png",
+            "floor_1.png",
+            "floor_2.png",
+            "floor_3.png",
+            "floor_4.png",
+            "floor_5.png",
+            "floor_6.png",
+            "floor_7.png",
+            "floor_8.png",
+            "floor_ladder.png",
+            "image_not_found.png",
+            "knight_f_idle_anim_f0.png",
+            "lizard_f_idle_anim_f0.png",
+            "wall_side_mid_right.png",
+            "wall_side_mid_left.png",
+            "wall_top_left.png",
+            "wall_top_right.png",
+            "wall_top_mid.png",
+            "wall_corner_left.png",
+            "wall_corner_right.png",
+            "wall_mid.png",
+            "wall_corner_bottom_left.png",
+            "wall_corner_bottom_right.png",
+            "wall_top_mid.png",
+            "wall_left.png",
+            "wall_right.png",
+            "wall_mid.png"};
+
 
     public static Map<String, BufferedImage> loadFiles() throws IOException {
         Map<String, BufferedImage> imageList = new HashMap<>();
 
-
-        BufferedImage image1 = loadByPath("images/characters/big_demon_idle_anim_f0.png");
-        imageList.put("big_demon_idle_anim_f0.png", image1);
-        BufferedImage image2 = loadByPath("images/default/image_not_found.png");
-        imageList.put("image_not_found.png", image2);
-
-
+        for (String imageName : imageNames) {
+            BufferedImage image = loadByPathAsStream("usedImages/" + imageName);
+            imageList.put(imageName, image);
+        }
         return imageList;
     }
 
     private static BufferedImage loadByPathAsStream(String path) throws IOException {
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = loader.getResourceAsStream(path);
-        System.out.println(inputStream);
         return ImageIO.read(inputStream);
     }
 
@@ -38,32 +63,6 @@ public class ResourceList {
         File file = new File(loader.getResource(path).getPath());
         return ImageIO.read(file);
     }
-
-
-
-    /*
-    public static Map<String, File> loadFiles() {
-        Map<String, File> imageList = new HashMap<>();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-       File tempFile = null;
-        try {
-            tempFile = File.createTempFile("temp", "image");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-
-        try {
-            FileUtils.copyInputStreamToFile(loader.getResourceAsStream("/images/characters/big_demon_idle_anim_f0.png"), tempFile);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        // loadSubdirectoryFiles(tempFile, imageList);
-        return imageList;
-    }
-    */
 
     private static void loadSubdirectoryFiles(final File f, final Map<String, File> imageList) {
         File[] files = f.listFiles();
